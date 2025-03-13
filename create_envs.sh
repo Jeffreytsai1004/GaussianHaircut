802c58121a525b3a57a82244d8a970e2470c3af3
Jeffreytsai1004 <jeffreytsai1004@gmail.com>
2025-03-14 02:40:59 +0800

    Update create_envs.sh

--- create_envs.sh
+++ create_envs.sh
@@ -15,7 +15,7 @@ pip install cmake lit
 pip install "pillow<11.0" --force-reinstall
 conda deactivate && conda activate base_01
 # Openpose
-conda create -y -n openpose python=3.8 pip cmake=3.31.5 numpy pillow protobuf -c conda-forge
+conda create -y -n openpose pip cmake=3.31.5 numpy pillow protobuf -c conda-forge
 conda activate openpose
 conda deactivate && conda activate base_01
 # PIXIE
